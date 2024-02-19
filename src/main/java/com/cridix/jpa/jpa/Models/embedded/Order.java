09015571d7768bf8defbe52331e2eeb38e0387a3
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-02-19 05:12:45 -0500

    Embeded

---
+++ src/main/java/com/cridix/jpa/jpa/Models/embedded/Order.java
@@ -0,0 +1,28 @@
+package com.cridix.jpa.jpa.Models.embedded;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @EmbeddedId
+    private OrderId id;
+
+    @Embedded
+    private Address address;
+
+    private String orderInfo;
+
+    private String anotherField;
+
+}
